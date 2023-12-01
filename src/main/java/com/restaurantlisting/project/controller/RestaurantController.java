0f62808a5711d3fe4d04f4bb0d924410e2b9270d
nageshgitversion <rasukuntanagesh@gmail.com>
2023-12-01 22:22:28 +0530

    build not work

--- src/main/java/com/restaurantlisting/project/controller/RestaurantController.java
+++ src/main/java/com/restaurantlisting/project/controller/RestaurantController.java
@@ -16,7 +16,7 @@ import com.restaurantlisting.project.service.RestaurantService;
 @RestController
 public class RestaurantController {
 	
-	String name ;
+	String name ="eswara";
 	
 	@Autowired
 	RestaurantService service;
